5081f7efeda53d93937499d145afdaa02e154a34
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2023-11-21 18:52:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function capitalizeName() {
+    var inputField = document.getElementById("fname");
+    inputField.value = inputField.value.toUpperCase();
+  }
+
+  // Add event listener to trigger capitalizeName function on blur (losing focus)
+  document.getElementById("fname").addEventListener("blur", capitalizeName);
